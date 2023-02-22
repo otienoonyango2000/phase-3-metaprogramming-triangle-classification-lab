bf9ed785614f548f5e1a6d32d37beee55433cb1b
otienoonyango2000 <otienoonyango2000@gmail.com>
2023-02-22 19:44:47 +0300

    triangle classification lab completed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
   # write code here
+
+  attr_accessor :type
+
+  def initialize(length1, length2, length3)
+    sides = [length1, length2, length3]
+    sides.each.with_index do |s, i|
+      if((sides[(i+1)%3] + sides[(i+2)%3]) <= s)
+        raise TriangleError
+      end
+    end
+
+    if(!!sides.find { |s| s <=0 })
+      raise TriangleError
+    end
+
+    if(sides.uniq.length == 1)
+      @type = "equilateral"
+    elsif(sides.uniq.length == 2)
+      @type = "isosceles"
+    else
+      @type = "scalene"
+    end
+  end
+
+  def kind
+    type.to_sym
+  end
+
+  class TriangleError < StandardError
+    def message
+    end
+  end
+
 end
+
+t1 = Triangle.new(10,11,12)
+t2 = Triangle.new(10,10,2)
+t3 = Triangle.new(10,10,10)
